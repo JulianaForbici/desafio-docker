d8ec875df83e99a032ec40b5f1a86991b8a34df0
Juliana Forbici <105084941+JulianaForbici@users.noreply.github.com>
2025-08-21 19:12:01 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -27,7 +27,6 @@ echo "Rebuilding and restarting containers..."
 sudo docker-compose down
 sudo docker-compose up --build -d --force-recreate
 
-# Create cron job to run every 5 minutes (no log redirection)
 crontab -l 2>/dev/null | grep -q "deploy_pizzaria.sh" || (crontab -l 2>/dev/null; echo "*/5 * * * * $PROJECT_DIR/deploy_pizzaria.sh") | cro>
 
 echo "Finished."
